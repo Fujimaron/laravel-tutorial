14044d537d201c72ca0acc8bd60e76bd47b9aa82
Fujimaron <kiwibird2020@yahoo.co.jp>
2020-12-06 20:11:46 +0900

    chapter5 finished

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,14 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController')->name('tasks.index');
 
+// 一覧画面
+Route::get('/folders/{id}/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.
+index');
 
-Route::get('/folders/{id}/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+// フォルダの作成
+Route::get('/folders/create', [App\Http\Controllers\FolderController::class, 'showCreateForm'])->name('folders.create');
 
+Route::post('/folders/create', [App\Http\Controllers\FolderController::class, 'create']);
 
 
 // Route::get(
