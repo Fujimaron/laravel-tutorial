dcc0c3f46bee3aa01aa4f83059573592f3a5a08c
Fujimaron <kiwibird2020@yahoo.co.jp>
2020-12-12 11:41:12 +0900

    chapter6 finished

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,26 +4,59 @@ namespace App\Http\Controllers;
 
 use App\Models\Folder;
 use App\Models\Task;
+use App\Http\Requests\CreateTask;
 use Illuminate\Http\Request;
 
 
 class TaskController extends Controller
 {
-        public function index(int $id)
-        {
-            // 全てのフォルダを取得する
-            $folders = Folder::all();
+    public function index(int $id)
+    {
+        // 全てのフォルダを取得する
+        $folders = Folder::all();
 
-            // 選ばれたフォルダを取得する
-            $current_folder = Folder::find($id);
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
 
-            // 選ばれたフォルダに紐づくタスクを取得する
-            $tasks = $current_folder->tasks()->get();
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
 
-            return view('tasks/index', [
-                'folders' => $folders,
-                'current_folder_id' => $id,
-                'tasks' => $tasks,
-            ]);
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+            'tasks' => $tasks,
+        ]);
     }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', ['folder_id' => $id]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+
 }
+
--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,30 @@ use Illuminate\Support\Facades\Route;
 
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 // Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController')->name('tasks.index');
 
 // 一覧画面
-Route::get('/folders/{id}/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.
-index');
+Route::get('/folders/{id}/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
 
 // フォルダの作成
 Route::get('/folders/create', [App\Http\Controllers\FolderController::class, 'showCreateForm'])->name('folders.create');
-
 Route::post('/folders/create', [App\Http\Controllers\FolderController::class, 'create']);
+
+// タスクの作成
+Route::get('/folders/{id}/tasks/create', [App\Http\Controllers\TaskController::class, 'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [App\Http\Controllers\TaskController::class, 'create']);
+
+// タスクの編集
+Route::get('/folders/{id}/tasks/{task_id}/edit', [App\Http\Controllers\TaskController::class, 'showEditForm'])->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', [App\Http\Controllers\TaskController::class, 'edit']);
+
+
+
 
 
 // Route::get(
